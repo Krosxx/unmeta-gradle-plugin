a3ccb32374bd6b66b525ed88c0db8262d3708063
Libra <1132412166@qq.com>
2023-07-28 10:05:49 +0800

    local pub

--- plugin-build/unmeta/build.gradle.kts
+++ plugin-build/unmeta/build.gradle.kts
@@ -27,66 +27,12 @@ tasks.withType<KotlinCompile> {
         jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 }
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            pom {
-                name.set(property("DISPLAY_NAME").toString())
-                description.set(property("DESCRIPTION").toString())
-                url.set(property("WEBSITE").toString())
-
-                organization {
-                    name.set(property("ORGANIZATION_NAME").toString())
-                    url.set(property("ORGANIZATION_URL").toString())
-                }
-
-                licenses {
-                    license {
-                        name.set(property("LICENSE_NAME").toString())
-                        url.set(property("LICENSE_URL").toString())
-                    }
-                }
-
-                scm {
-                    val (vcsHost, vcsUser, vcsRepo) = property("VCS_URL").toString()
-                        .substringAfter("https://")
-                        .split("/")
-                    url.set(property("VCS_URL").toString())
-                    connection.set("scm:git:git://$vcsHost/$vcsUser/$vcsRepo.git")
-                    developerConnection.set("scm:git:ssh://git@$vcsHost:$vcsUser/$vcsRepo.git")
-                }
-
-                developers {
-                    developer {
-                        name.set(property("DEVELOPER_NAME").toString())
-                    }
-                }
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = System.getenv("SONATYPE_USERNAME")
-                password = System.getenv("SONATYPE_SECRET")
-            }
-        }
-    }
-}
-
-//signing {
-//    val signingKey = System.getenv("GPG_SIGNING_KEY")
-//    val signingPassword = System.getenv("GPG_SIGNING_PASSWORD")
-//    useInMemoryPgpKeys(signingKey, signingPassword)
-//    sign(tasks["jar"])
-//    sign(publishing.publications["mavenJava"])
-//}
+version = property("VERSION") ?: ""
+group = property("GROUP") ?: ""
 
 gradlePlugin {
+    website.set(property("WEBSITE").toString())
+    vcsUrl.set(property("VCS_URL").toString())
     plugins {
         create(property("ID").toString()) {
             id = property("ID").toString()
@@ -99,21 +45,3 @@ gradlePlugin {
     }
 }
 
-gradlePlugin {
-    website.set(property("WEBSITE").toString())
-    vcsUrl.set(property("VCS_URL").toString())
-}
-
-tasks.create("setupPluginUploadFromEnvironment") {
-    doLast {
-        val key = System.getenv("GRADLE_PUBLISH_KEY")
-        val secret = System.getenv("GRADLE_PUBLISH_SECRET")
-
-        if (key == null || secret == null) {
-            throw GradleException("gradlePublishKey and/or gradlePublishSecret are not defined environment variables")
-        }
-
-        System.setProperty("gradle.publish.key", key)
-        System.setProperty("gradle.publish.secret", secret)
-    }
-}
